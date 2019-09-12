422a8c45a6fd03e487f797fb01864930d5567283
Matthew Wilkes <tonicblue@gmail.com>
2019-09-12 16:47:58 +0100

    Added base deployment shell script

--- deploy.sh
+++ deploy.sh
@@ -1,27 +1,31 @@
 #!/usr/bin/env bash
 
-if [[ `git status --porcelain` ]]; then
-  echo "Changes"
-else
-  echo "No changes"
-fi
+UPSTREAM=${1:-'@{u}'}
+LOCAL=$(git rev-parse @)
+REMOTE=$(git rev-parse "$UPSTREAM")
+BASE=$(git merge-base @ "$UPSTREAM")
 
-# travis encrypt-file secret.json --add --force
-#
-# git add secret.json.enc .travis.yml
-#
-# git commit -m \"Updated secret.json\"
-#
-# npm version patch
-#
-# git push
-#
-# git checkout production
-#
-# git pull
-#
-# git pull origin development
-#
-# git push
-#
-# git checkout development
+if [ $(git rev-parse --abbrev-ref HEAD) != \"development\" ]; then
+  echo "Cannot deploy: You can only deploy from the 'development' branch"
+elif [ $LOCAL = $REMOTE ]; then
+  echo "DEPLOYING"
+
+  # travis encrypt-file secret.json --add --force
+  # git add secret.json.enc .travis.yml
+  # git commit -m "Updated secret.json"
+  # npm version patch
+  # git push
+  # git checkout production
+  # git pull
+  # git pull origin development
+  # git push
+  # git checkout development
+elif [ $LOCAL = $BASE ]; then
+  echo "Cannot deploy: Need to pull"
+elif [ $REMOTE = $BASE ]; then
+  echo "Cannot deploy: Need to push"
+elif [[ `git status --porcelain` ]]; then
+  echo "Cannot deploy: There are local changes"
+else
+  echo "Cannot deploy: Diverged"
+fi
