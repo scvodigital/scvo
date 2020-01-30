ea8f80e33a142735e86c46609f602968fd32b050
Calum MacUisdean <calum.macuisdean@gmail.com>
2020-01-30 10:01:07 +0000

    Refactoring and template fixes

--- assets/post.js
+++
@@ -1,4 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var menu = new mdc.menu.MDCMenu(document.querySelector('#share-menu'));
-    document.querySelector('#share-menu-button').addEventListener('click', () => menu.open = !menu.open);
-});
