ab24f3d3147aa2e192ba3de6cd8cb7256330df6c
Calum MacUisdean <calum.macuisdean@gmail.com>
2020-04-14 09:25:58 +0100

    new method for highlighting last updated dates

--- assets/js/accordion.js
+++ assets/js/accordion.js
@@ -11,35 +11,37 @@ for (i = 0; i < acc.length; i++) {
     }
   });
 }
-
-document.getElementById("accordions_expand").addEventListener("click", function() {
-  var acc = document.getElementsByClassName("accordion");
-  var i;
-  for (i = 0; i < acc.length; i++) {
-    if (acc[i].classList.value.includes('active') != true) {
-      acc[i].classList.add("active");
-      var panel = acc[i].nextElementSibling;
-      if (panel.style.maxHeight) {
-        panel.style.maxHeight = null;
-      } else {
-        panel.style.maxHeight = panel.scrollHeight + "px";
+if (document.getElementById("accordions_expand")) {
+  document.getElementById("accordions_expand").addEventListener("click", function() {
+    var acc = document.getElementsByClassName("accordion");
+    var i;
+    for (i = 0; i < acc.length; i++) {
+      if (acc[i].classList.value.includes('active') != true) {
+        acc[i].classList.add("active");
+        var panel = acc[i].nextElementSibling;
+        if (panel.style.maxHeight) {
+          panel.style.maxHeight = null;
+        } else {
+          panel.style.maxHeight = panel.scrollHeight + "px";
+        }
       }
     }
-  }
-});
-
-document.getElementById("accordions_collapse").addEventListener("click", function() {
-  var acc = document.getElementsByClassName("accordion");
-  var i;
-  for (i = 0; i < acc.length; i++) {
-    if (acc[i].classList.value.includes('active')) {
-      acc[i].classList.remove("active");
-      var panel = acc[i].nextElementSibling;
-      if (panel.style.maxHeight) {
-        panel.style.maxHeight = null;
-      } else {
-        panel.style.maxHeight = panel.scrollHeight + "px";
+  });
+}
+if (document.getElementById("accordions_collapse")) {
+  document.getElementById("accordions_collapse").addEventListener("click", function() {
+    var acc = document.getElementsByClassName("accordion");
+    var i;
+    for (i = 0; i < acc.length; i++) {
+      if (acc[i].classList.value.includes('active')) {
+        acc[i].classList.remove("active");
+        var panel = acc[i].nextElementSibling;
+        if (panel.style.maxHeight) {
+          panel.style.maxHeight = null;
+        } else {
+          panel.style.maxHeight = panel.scrollHeight + "px";
+        }
       }
     }
-  }
-});
+  });
+}
